6a496b30a99deec93e649857bc590303bc0fabef
Saurabh Pathak <saurabh.pathak@optimusinfo.com>
2015-06-03 11:42:05 +0530

    removing unnecessary files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,14 +23,6 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .when('/myroute', {
-        templateUrl: 'views/myroute.html',
-        controller: 'MyrouteCtrl'
-      })
       .otherwise({
         redirectTo: '/'
       });
