975fbdeeb2a1a5a5b81b6c6068937a14031bc43f
Mohit Raj Purohit <mohit.raj.purohit@optimusinfo.com>
2015-06-17 16:23:19 +0530

    Change Module name

--- app/scripts/controllers/slider.js
+++ app/scripts/controllers/slider.js
@@ -7,7 +7,7 @@
  * # SliderCtrl
  * Controller of the html5Application1App
  */
-angular.module('html5Application1App')
+angular.module('yeomanProject')
   .controller('SliderCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
