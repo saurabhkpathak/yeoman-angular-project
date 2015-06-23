0017aa956694c226bb5a94414d78544abd588b3f
Saurabh Pathak <saurabh.pathak@optimusinfo.com>
2015-06-23 18:09:42 +0530

    adding functionality to add cards

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,30 @@
  * Controller of the yumitzApp
  */
 angular.module('yumitzApp')
-  .controller('MainCtrl', function ($rootScope, $location) {
-      if (!$rootScope.isAuthorised) {
-          $location.path('/');
+  .controller('MainCtrl', function ($scope, $rootScope, $location) {
+      // if (!$rootScope.isAuthorised) {
+      //     $location.path('/');
+      // }
+      function Card() {
+        this.orderNum = null;
+        this.quantity = 0;
+        this.locality = null;
+      }
+      $scope.cards = [];
+      $scope.createNewCard = function() {
+        debugger;
+        if (isPreviousCardNull($scope.cards)) {
+          $scope.cards.push(new Card());
+        } else {
+          console.warn('previous is empty');
+        }
+      };
+      function isPreviousCardNull(list) {
+        if (list.length === 0) {
+          return true;
+        } else if (list[list.length - 1] === Card) {
+          return false;
+        }
+        return true;
       }
   });
