e64245ada77df7d90daa2a6ae8c755405434fc89
ankit <ankitrastogi.12@gmail.com>
2015-07-01 17:48:31 +0530

    Internal QA Fix

--- app/scripts/controllers/slider.js
+++ app/scripts/controllers/slider.js
@@ -8,71 +8,71 @@
  * Controller of the html5Application1App
  */
 angular.module('yeomanProject')
-  .controller('SliderCtrl', function ($scope) {
+  .controller('SliderCtrl', function($scope) {
     // $scope.awesomeThings = [
     //   'HTML5 Boilerplate',
     //   'AngularJS',
     //   'Karma'
     // ];
-  }).directive('jCaraousel', function() {
+  }).directive('jCaraousel', function($timeout) {
     return {
       restrict: 'E',
       scope: {
-          sliderContent: '='
-        },
+        sliderContent: '='
+      },
       templateUrl: '../../views/slider.html',
-      link: function(scope) {
-          scope.$watch('scope.sliderContent', function() {
-              (function($) {
-                $(function() {
-                  var jcarousel = $('.jcarousel');
+      link: function() {
 
-                  jcarousel
-                  .on('jcarousel:reload jcarousel:create', function() {
-                    var carousel = $(this),
+        $timeout(function() {
+          (function($) {
+            $(function() {
+              var jcarousel = $('.jcarousel');
+
+              jcarousel
+                .on('jcarousel:reload jcarousel:create', function() {
+                  var carousel = $(this),
                     width = carousel.innerWidth();
-                    if (width >= 600) {
-                      width = width / 4;
-                    } else if (width >= 350) {
-                      width = width / 2;
-                    }
+                  if (width >= 600) {
+                    width = width / 4;
+                  } else if (width >= 350) {
+                    width = width / 2;
+                  }
 
-                    carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
-                  })
-                  .jcarousel({
-                    wrap: 'circular'
-                  });
-
-                  $('.jcarousel-control-prev')
-                  .jcarouselControl({
-                    target: '-=1'
-                  });
-
-                  $('.jcarousel-control-next')
-                  .jcarouselControl({
-                    target: '+=1'
-                  });
-
-                  $('.jcarousel-pagination')
-                  .on('jcarouselpagination:active', 'a', function() {
-                    $(this).addClass('active');
-                  })
-                  .on('jcarouselpagination:inactive', 'a', function() {
-                    $(this).removeClass('active');
-                  })
-                  .on('click', function(e) {
-                    e.preventDefault();
-                  })
-                  .jcarouselPagination({
-                    perPage: 1,
-                    item: function(page) {
-                      return '<a href="#' + page + '">' + page + '</a>';
-                    }
-                  });
+                  carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+                })
+                .jcarousel({
+                  wrap: 'circular'
                 });
-              })(jQuery);
-        });
 
+              $('.jcarousel-control-prev')
+                .jcarouselControl({
+                  target: '-=1'
+                });
+
+              $('.jcarousel-control-next')
+                .jcarouselControl({
+                  target: '+=1'
+                });
+
+              $('.jcarousel-pagination')
+                .on('jcarouselpagination:active', 'a', function() {
+                  $(this).addClass('active');
+                })
+                .on('jcarouselpagination:inactive', 'a', function() {
+                  $(this).removeClass('active');
+                })
+                .on('click', function(e) {
+                  e.preventDefault();
+                })
+                .jcarouselPagination({
+                  perPage: 1,
+                  item: function(page) {
+                    return '<a href="#' + page + '">' + page + '</a>';
+                  }
+                });
+            });
+          })(jQuery);
+        }, 500);
       }
     }
   });
