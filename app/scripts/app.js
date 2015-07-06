03664571a178fecc1e1615ab61e41a7d3b4f233a
Mohit Raj Purohit <mohit.raj.purohit@optimusinfo.com>
2015-07-06 18:56:05 +0530

    calender Angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,13 +16,55 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ui.sortable'
+    'ui.sortable',
+    'ui.bootstrap',
+    'ui.bootstrap.datetimepicker'
 ]).controller('mainController', function($scope, $location, $route) {
     $scope.$on('$routeChangeSuccess', function(newVal, oldVal) {
         if (oldVal !== newVal) {
             $scope.routeClassName = $route.current.className;
         }
     });
+
+
+
+
+    $scope.dates = {
+      date1: new Date()
+    };
+
+    $scope.open = {
+      date1: false
+    };
+
+    // Disable weekend selection
+    // $scope.disabled = function(date, mode) {
+    //   return (mode === 'day' && (new Date().toDateString() == date.toDateString()));
+    // };
+
+    // $scope.dateOptions = {
+    //   showWeeks: false,
+    //   startingDay: 1
+    // };
+
+    $scope.openCalendar = function(e, date) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      $scope.open[date] = true;
+    };
+
+    // watch date4 and date5 to calculate difference
+    // $scope.$watch(function() {
+    //   return $scope.dates;
+    // }, function() {
+    //   if ($scope.dates.date4 && $scope.dates.date5) {
+    //     var diff = $scope.dates.date4.getTime() - $scope.dates.date5.getTime();
+    //     $scope.dayRange = Math.round(Math.abs(diff/(1000*60*60*24)))
+    //   } else {
+    //     $scope.dayRange = 'n/a';
+    //   }
+    // }, true);
 
 }).config(function ($routeProvider) {
     $routeProvider
