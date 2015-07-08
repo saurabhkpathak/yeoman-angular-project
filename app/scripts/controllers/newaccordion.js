0f0f9244b56b351922c4b160825c84d23853453a
himanshuk-optimus <himashu.kumar@optimusinfo.com>
2015-07-08 16:24:18 +0530

    dsfdsfsdf

--- app/scripts/controllers/newaccordion.js
+++ app/scripts/controllers/newaccordion.js
@@ -35,62 +35,62 @@ angular.module('yeomanProject')
   //   isFirstDisabled: false
   // };
 
-  scope.items = [
-  {
-    name: "item1",
-    desc: "Item 1",
-    subitems: [
-    {
-      name: "subitem1",
-      desc: "Sub-Item 1"
-    },
-    {
-      name: "subitem2",
-      desc: "Sub-Item 2"
-    },
-    {
-      name: "subitem2",
-      desc: "Sub-Item 2"
-    }]
-  },
-  {
-    name: "item2",
-    desc: "Item 2",
-    subitems: [
-    {
-      name: "subitem1",
-      desc: "Sub-Item 1"
-    },
-    {
-      name: "subitem2",
-      desc: "Sub-Item 2"
-    },
-    {
-      name: "subitem2",
-      desc: "Sub-Item 2"
-    }]
-  },
-  {
-    name: "item3",
-    desc: "Item 3",
-    subitems: [
-    {
-      name: "subitem1",
-      desc: "Sub-Item 1"
-    },
-    {
-      name: "subitem2",
-      desc: "Sub-Item 2"
-    },
-    {
-      name: "subitem2",
-      desc: "Sub-Item 2"
-    }]
-  }
-  ];
-
-  scope.default = scope.items[2];
-
+  // scope.items = [
+  // {
+  //   name: "item1",
+  //   desc: "Item 1",
+  //   subitems: [
+  //   {
+  //     name: "subitem1",
+  //     desc: "Sub-Item 1"
+  //   },
+  //   {
+  //     name: "subitem2",
+  //     desc: "Sub-Item 2"
+  //   },
+  //   {
+  //     name: "subitem2",
+  //     desc: "Sub-Item 2"
+  //   }]
+  // },
+  // {
+  //   name: "item2",
+  //   desc: "Item 2",
+  //   subitems: [
+  //   {
+  //     name: "subitem1",
+  //     desc: "Sub-Item 1"
+  //   },
+  //   {
+  //     name: "subitem2",
+  //     desc: "Sub-Item 2"
+  //   },
+  //   {
+  //     name: "subitem2",
+  //     desc: "Sub-Item 2"
+  //   }]
+  // },
+  // {
+  //   name: "item3",
+  //   desc: "Item 3",
+  //   subitems: [
+  //   {
+  //     name: "subitem1",
+  //     desc: "Sub-Item 1"
+  //   },
+  //   {
+  //     name: "subitem2",
+  //     desc: "Sub-Item 2"
+  //   },
+  //   {
+  //     name: "subitem2",
+  //     desc: "Sub-Item 2"
+  //   }]
+  // }
+  // ];
+  //
+  // scope.default = scope.items[2];
+// debugger
   scope.$parent.isopen = (scope.$parent.default === scope.item);
 
   scope.$watch('isopen', function (newvalue, oldvalue, scope) {
